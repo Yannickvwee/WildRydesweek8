ae65461aa5ca56d6949b9a68c25ab3dac64e9f6f
Yannickvwee <yannickvanwee@gmail.com>
2020-10-25 23:28:29 +0100

    Update ride.js

--- js/ride.js
+++ js/ride.js
@@ -20,7 +20,8 @@ WildRydes.map = WildRydes.map || {};
             method: 'POST',
             url: _config.api.invokeUrl + '/RequestUnicorn',
             headers: {
-                Authorization: authToken
+                Authorization: authToken,
+				Access-Control-Allow-Origin: '*'
             },
             data: JSON.stringify({
                 PickupLocation: {
