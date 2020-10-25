bbb16ea68fd94bc80cfe1425d33eb3b773492798
Yannickvwee <yannickvanwee@gmail.com>
2020-10-25 20:31:27 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_n5YnJw1Hq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'i1db49b3r9et82mnrdfgtdgir', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'v1qhk01ssk151ggqnodmpjq3p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
